73fac1688949b76956f07f5481dec762363474b8
Sandro Simic <sandrosimic8@gmail.com>
2023-12-28 19:23:48 +0100

    ,

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import connectDB from "./config/db.js";
 import AppError from "./utils/appError.js";
 import globalErrorHandler from "./controllers/errorController.js";
 import cors from "cors";
+import path from 'path'
+
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -29,13 +31,27 @@ app.use(cors(corsOptions));
 app.use("/api/cars", carsRouter);
 app.use("/api/users", usersRouter);
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+const currentFilePath = new URL(import.meta.url).pathname;
+const currentDirPath = path.dirname(currentFilePath);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(currentDirPath, '/client/dist')))
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(currentDirPath, 'client', 'dist', 'index.html'))
+  })
+}
+else {
+  app.get('/', (req,res) => {
+    res.send('Api is running....')
+  })
+}
 
 app.use(globalErrorHandler);
 
