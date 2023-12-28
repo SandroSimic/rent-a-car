6ec2facc7dc1614fcfc285504c6dcaa30988a77d
Sandro Simic <sandrosimic8@gmail.com>
2023-12-28 19:47:45 +0100

    final commit 2

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://renlty.onrender.com"];
 const corsOptions = {
   origin: allowedOrigins,
   credentials: true,
