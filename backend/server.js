c85185a087fcf46f1215f69a01b52de08052a7a7
Sandro Simic <sandrosimic8@gmail.com>
2023-12-28 20:40:13 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -7,45 +7,39 @@ import connectDB from "./config/db.js";
 import AppError from "./utils/appError.js";
 import globalErrorHandler from "./controllers/errorController.js";
 import cors from "cors";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path'
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 
 dotenv.config();
 
-
 connectDB();
-
 const app = express();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 app.use(cookieParser());
 
-app.use(cors({credentials: true, origin: ["http://localhost:5173", "https://renlty.onrender.com"]}));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "https://renlty.onrender.com"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/cars", carsRouter);
 app.use("/api/users", usersRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-
-
-
-
-if (process.env.NODE_ENV === 'production') {
-  const clientDistPath = path.join(__dirname, '../client/dist'); 
-
-  app.use(express.static(clientDistPath));
-
-  app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.resolve(clientDistPath, 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
+  app.get("/", (req, res) => {
+    res.send("API is running....");
   });
 }
 
@@ -54,7 +48,6 @@ app.all("*", (req, res, next) => {
 });
 
 app.use(globalErrorHandler);
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, (req, res) => {
